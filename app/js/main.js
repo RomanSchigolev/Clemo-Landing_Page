ffcbff14e2e910c560e7de537626063b8d379add
Roman Schigolev <r.shchigolev2018@yandex.ru>
2019-08-28 17:01:41 +0700

    section clients is almost ready

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+// document.body.onclick = function(event){
+//     //console.log(event);
+//     const className = event.target.classList;
+//     console.log(className);
+//
+//     if ( className.indexOf('item__photo__img') !== -1 ){
+//         console.log('yep');
+//     }
+// };
