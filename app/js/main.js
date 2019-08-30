7cafe3075a183f62847f3485d9a460c8e9c407f0
Roman Schigolev <r.shchigolev2018@yandex.ru>
2019-08-30 14:00:40 +0700

    the effect of the appearance of the text in the header

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,21 @@
-function appearText(){
+function appearDescriptionText(){
     const text = document.querySelectorAll('.services__description');
     text.forEach(item => {
        let textPosition = item.getBoundingClientRect().top;
        let screenPosition = window.innerHeight;
-       if(textPosition < screenPosition / 2){
-           item.classList.add('appear');
+       if(textPosition < screenPosition / 1.5){
+           item.classList.add('description--appear');
        }
     });
 }
-window.addEventListener('scroll', appearText);
+function appearHeader(){
+    const logo = document.querySelector('.logo');
+    logo.classList.add('logo--appear');
+    const menuLink = document.querySelectorAll('.menu__item');
+    menuLink.forEach(item => {
+        item.classList.add('item--appear');
+    });
+}
+appearHeader();
+window.addEventListener('scroll', appearDescriptionText);
 
