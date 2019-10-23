065e748a26f6f1fa929d90d78e44ed551f5045fd
Roman Schigolev <r.shchigolev2018@yandex.ru>
2019-10-23 23:57:34 +0700

    added babel

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,23 @@
-function appearDescriptionText(){
-    const text = document.querySelectorAll('.services__description');
-    text.forEach(item => {
-       let textPosition = item.getBoundingClientRect().top;
-       let screenPosition = window.innerHeight;
-       if(textPosition < screenPosition / 1.5){
-           item.classList.add('description--appear');
-       }
-    });
+function appearDescriptionText() {
+  const text = document.querySelectorAll('.services__description');
+  text.forEach(item => {
+    let textPosition = item.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight;
+    if (textPosition < screenPosition / 1.5) {
+      item.classList.add('description--appear');
+    }
+  });
 }
-function appearHeader(){
-    const logo = document.querySelector('.logo');
-    logo.classList.add('logo--appear');
-    const menuLink = document.querySelectorAll('.menu__item');
-    menuLink.forEach(item => {
-        item.classList.add('item--appear');
-    });
+
+function appearHeader() {
+  const logo = document.querySelector('.logo');
+  logo.classList.add('logo--appear');
+  const menuLink = document.querySelectorAll('.menu__item');
+  menuLink.forEach(item => {
+    item.classList.add('item--appear');
+  });
 }
+
 appearHeader();
 window.addEventListener('scroll', appearDescriptionText);
 
